b21d0f2bca53da57505261bc5897abba9a8b84ae
Ivanildo Ferreira <isivanildo@hotmail.com>
2022-08-22 20:54:42 -0300

    Criação dos modelos de dados e do DbContext

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APICatalogo.Models;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        DbSet<Categoria>? Categorias { get; set; }
+        DbSet<Produto>? Produtos { get; set; }
+    }
+}
